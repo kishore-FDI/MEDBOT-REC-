8ea33b8a6c7f037119224438682f03b2a84bba90
Kishore-kumar <147427164+kishore-FDI@users.noreply.github.com>
2023-10-25 18:51:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,66 @@ from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 import os
+from langchain import PromptTemplate
 
 os.environ['GOOGLE_API_KEY'] = 'AIzaSyBwZZBnDZXJUrqF7f-m-m0zxT3fYFtcQB8'
 
+custom_prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Try to answer in points.
 
-def get_vector_store():
-    embeddings = GooglePalmEmbeddings()
-    vector_store = FAISS.load_local("faiss_index", embeddings)
-    return vector_store
+Context: {context}
+Question: {question}
+
+Return the answer below with the explanation in simple words with an example and deep knowledge.
+Answer with explanation in simple words:
+"""
 
 
-def get_conversational_chain(vector_store):
-    llm = GooglePalm(temperature=0.1)
+def set_custom_prompt():
+    """
+    Prompt template for QA retrieval for each vector store
+    """
+    prompt = PromptTemplate(template=custom_prompt_template,
+                            input_variables=['context', 'question'])
+    return prompt
+
+
+# def get_vector_store():
+#     embeddings = GooglePalmEmbeddings()
+#     vector_store = FAISS.load_local("faiss_index", embeddings)
+#     return vector_store
+
+
+def retrieval_qa_chain(prompt,db):
+    llm = GooglePalm(temperature=0.001)
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(),
-                                                               memory=memory)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm,
+                                                               retriever=db.as_retriever(search_kwargs={"k": 10}),
+                                                               memory=memory,
+                                                               prompt=prompt)
     return conversation_chain
+
+
+def qa_bot():
+    embeddings = GooglePalmEmbeddings()
+    db = FAISS.load_local("faiss_index", embeddings)
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(qa_prompt, db)
+    return qa
+
+
+if 'conversation_history' not in st.session_state:
+    st.session_state['conversation_history'] = []
+# Define sidebar content
+st.sidebar.title("Sample Questions")
+st.sidebar.write("1. What are the symptoms of COVID-19?")
+st.sidebar.write("2. How can I lower my blood pressure?")
+st.sidebar.write("3. Tell me about diabetes management.")
+st.sidebar.write("4. How to treat a common cold?")
+st.sidebar.write("5. Describe the signs of a heart attack.")
+st.sidebar.write("6. Contact Details:")
+st.sidebar.write("   Email: kishore22705@gmail.com.com")
 
 
 def user_input(user_question):
@@ -36,27 +80,27 @@ def user_input(user_question):
 
 
 def main():
-    st.set_page_config("MedChatBot")
+    st.set_page_config("LLM-project")
     st.header("Medical-ChatBot")
-    with st.sidebar:
-        st.title("Thank you for using our bot💖")
-        st.subheader("PLEASE Click on Start")
+    for message in st.session_state['conversation_history']:
+        if message['role'] == 'user':
+            st.text(f"User: {message['content']}")
+        else:
+            st.text(f"Bot: {message['content']}")
 
-        if st.button("Start"):
-            with st.spinner("Processing"):
-                vector_store = get_vector_store()
-                st.session_state.conversation = get_conversational_chain(vector_store)
-                st.success("Done")
-        st.subheader("For any queries , contact me at Kishore22705@gmail.com")
+        # User input at the bottom
+    user_query = st.text_input("Enter your medical query:")
 
-    config = "Hello , Lets start solving your issue"
-    user_question = st.text_input("How can i assist you today?")
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chatHistory = None
-    if user_question:
-        user_input(user_question)
+    if st.button("Submit"):
+        # Get the chatbot's response
+        response = qa_bot()({'question': user_query})
+
+        # Store the conversation history
+        st.session_state['conversation_history'].append({"role": "user", "content": user_query})
+        st.session_state['conversation_history'].append({"role": "assistant", "content": response['answer']})
+        # Use st.empty() to update the response
+        response_placeholder = st.empty()
+        response_placeholder.text(f"Bot: {response['answer']}")
 
 
 if __name__ == "__main__":
