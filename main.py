24a94644974888fce5562945fc2eef5ecef28273
Kishore-kumar <147427164+kishore-FDI@users.noreply.github.com>
2023-11-02 22:15:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,8 @@ def retrieval_qa_chain(llm, prompt, db):
     return conversation_chain
 
 def qa_bot():
-    embeddings = GooglePalmEmbeddings()
+    embeddings =HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                       model_kwargs={'device': 'cpu'})
     db = FAISS.load_local(faiss_index, embeddings)
     llm = GooglePalm(temperature=0.001)
     qa_prompt = set_custom_prompt()
