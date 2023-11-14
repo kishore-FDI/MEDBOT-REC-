f33f176e6e80c7293778b77a63918720997d9df7
Kishore-kumar <147427164+kishore-FDI@users.noreply.github.com>
2023-11-14 23:41:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def retrieval_qa_chain(llm, prompt, db):
 def qa_bot():
     embeddings =GooglePalmEmbeddings()
     db = FAISS.load_local('faiss_index', embeddings)
-    llm = GooglePalm(temperature=0.001)
+    llm = GooglePalm(temperature=0.1)
     qa_prompt = set_custom_prompt()
     qa = retrieval_qa_chain(llm, qa_prompt, db)
     return qa
